83a2426ea8abe3bb8b94af94dbd212d155c67879
BestProductsAlways <127046853+BestProductsAlways@users.noreply.github.com>
2023-12-19 14:29:59 -0330

    Update UserDao.java

--- UserDao.java
+++ UserDao.java
@@ -3,38 +3,120 @@ import org.mindrot.jbcrypt.BCrypt;
 import java.sql.*;
 
 public class UserDao {
-   /*
+    private static final String JDBC_URL = "jdbc:postgresql://your_database_url";
+    private static final String JDBC_USERNAME = "Raymond_Payne";
+    private static final String JDBC_PASSWORD = "JuliaHoggi31956";
+
     public boolean createUser(User user) {
-        // insert user into database 
+        // insert user into database
         String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
 
-        // Prepare the SQL query
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "INSERT INTO users (first_name, last_name, email, password, is_doctor) VALUES (?, ?, ?, ?, ?)",
+                     Statement.RETURN_GENERATED_KEYS)) {
 
-        // Database logic to insert data using PREPARED Statement
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, hashedPassword);
+            preparedStatement.setBoolean(5, user.isDoctor());
 
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows > 0) {
+                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    user.setId(generatedKeys.getInt(1));
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
+        return false;
     }
-    public User getUserById(int id) { //get user by id from database 
+
+    public User getUserById(int id) {
         User user = null;
 
-        // Prepare the SQL query
-        // Database logic to get data by ID Using Prepared Statement
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE id = ?")) {
 
+            preparedStatement.setInt(1, id);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    user = extractUserFromResultSet(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
+        return user;
     }
 
-    public User getUserByEmail(String email) { // get user by email from database 
+    public User getUserByEmail(String email) {
         User user = null;
 
-        // Prepare the SQL query
-        // Database logic to get data by ID Using Prepared Statement
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE email = ?")) {
 
+            preparedStatement.setString(1, email);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    user = extractUserFromResultSet(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
+        return user;
     }
-
 
     public boolean updateUser(User user) {
-        // Prepare the SQL query
-        // Database logic to get update user Using Prepared Statement
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "UPDATE users SET first_name = ?, last_name = ?, email = ?, password = ?, is_doctor = ? WHERE id = ?")) {
+
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+            preparedStatement.setBoolean(5, user.isDoctor());
+            preparedStatement.setInt(6, user.getId());
+
+            int affectedRows = preparedStatement.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
+        return false;
     }
-    public boolean deleteUser(int id) { // delete user from the database 
-        // Prepare the SQL query
-        // Database logic to delete user
+
+    public boolean deleteUser(int id) {
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
+
+            preparedStatement.setInt(1, id);
+
+            int affectedRows = preparedStatement.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
+        return false;
     }
+
+    private User extractUserFromResultSet(ResultSet resultSet) throws SQLException {
+        return new User(
+                resultSet.getInt("id"),
+                resultSet.getString("first_name"),
+                resultSet.getString("last_name"),
+                resultSet.getString("email"),
+                resultSet.getString("password"),
+                resultSet.getBoolean("is_doctor")
+        );
+    }
+}
+    
