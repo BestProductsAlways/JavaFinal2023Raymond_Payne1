c40eafecc7907e090942e98aeedf8b991653eedc
BestProductsAlways <127046853+BestProductsAlways@users.noreply.github.com>
2023-12-20 15:39:01 -0330

    Create TestLoginUser.java

---
+++ TestLoginUser.java
@@ -0,0 +1,9 @@
+// TestLoginUser.java
+
+public class TestLoginUser {
+
+    public static void main(String[] args) {
+        System.out.println("=== Testing Login User ===");
+        HealthMonitoringApp.testLoginUser();
+    }
+}
