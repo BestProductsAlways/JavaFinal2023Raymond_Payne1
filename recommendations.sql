228573420b6f8f64cb2e385731dbb5a4239a3472
BestProductsAlways <127046853+BestProductsAlways@users.noreply.github.com>
2023-12-20 08:57:02 -0330

    Update recommendations.sql

--- recommendations.sql
+++ recommendations.sql
@@ -1,10 +1,10 @@
-CREATE TABLE medicine_reminders (
+CREATE TABLE health_data (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    medicine_name VARCHAR(100) NOT NULL,
-    dosage VARCHAR(50) NOT NULL,
-    schedule VARCHAR(100) NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
+    weight DECIMAL(5,2) NOT NULL,
+    height DECIMAL(5,2) NOT NULL,
+    steps INT NOT NULL,
+    heart_rate INT NOT NULL,
+    date DATE NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 )
