e301de7f21ca3d47786326b4fc9506922dae5cad
BestProductsAlways <127046853+BestProductsAlways@users.noreply.github.com>
2023-12-20 15:40:02 -0330

    Create TestDoctorPortal.java

---
+++ TestDoctorPortal.java
@@ -0,0 +1,9 @@
+// TestDoctorPortal.java
+
+public class TestDoctorPortal {
+
+    public static void main(String[] args) {
+        System.out.println("=== Testing Doctor Portal ===");
+        HealthMonitoringApp.testDoctorPortal();
+    }
+}
