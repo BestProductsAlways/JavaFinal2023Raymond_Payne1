be53ae9fb117d7098d527b90b7c8f145d7df5cc6
BestProductsAlways <127046853+BestProductsAlways@users.noreply.github.com>
2023-12-20 15:38:22 -0330

    Create TestRegisterNewUser.java

---
+++ TestRegisterNewUser.java
@@ -0,0 +1,9 @@
+// TestRegisterNewUser.java
+
+public class TestRegisterNewUser {
+
+    public static void main(String[] args) {
+        System.out.println("=== Testing Register New User ===");
+        HealthMonitoringApp.testRegisterNewUser();
+    }
+}
