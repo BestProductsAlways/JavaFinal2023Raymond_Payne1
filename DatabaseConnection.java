cea1dfb27e7bc371e1834a950352bb73ac7ca806
BestProductsAlways <127046853+BestProductsAlways@users.noreply.github.com>
2023-12-20 16:27:14 -0330

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -3,20 +3,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    // private static final String url = "jdbc:postgresql://localhost:5432/APHealthMonitoringApp";
-    private static final String url = "jdbc:postgresql://localhost:5432/aphealthmonitoringapp";
-    private static final String user = "Raymond_Payne";
-    private static final String password = "JuliaHoggi31956";
+    private static String url = "jdbc:postgresql://localhost:5432/aphealthmonitoringapp";
+    private static String user = "Raymond_Payne";
+    private static String password = "JuliaHoggi31956";
 
+    // Private constructor to prevent instantiation
     private DatabaseConnection() {
-        // private constructor to prevent instantiation
+        // You can add initialization logic here if needed
+    }
+
+    public static void setConnectionParameters(String newUrl, String newUser, String newPassword) {
+        // Allow dynamic changes to connection parameters
+        url = newUrl;
+        user = newUser;
+        password = newPassword;
     }
 
     public static Connection getCon() {
         Connection connection = null;
         try {
             Class.forName("org.postgresql.Driver"); // For Postgres
-//          Class.forName("com.mysql.jdbc.Driver");  // For MySQL
             connection = DriverManager.getConnection(url, user, password);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
